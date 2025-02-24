7f9bd3d686144237613dd532e387915ef4e6352a
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-02-24 19:53:20 +0200

    create SlidingSlogan

---
+++ lib/features/splash/presentation/views/widgets/sliding_slogan.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SlidingSlogan extends StatelessWidget {
+  const SlidingSlogan({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Free Books',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
