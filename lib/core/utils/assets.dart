d7220a518e8284aaa2ab98606dd2e27f645eeb57
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-02-25 22:01:55 +0200

    add test image path

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,4 +2,6 @@ class AssetsData {
   static const String logo = 'assets/images/Logo.svg';
 
   static const String search = 'assets/images/search.svg';
+
+  static const String testImage = 'assets/images/test_image.png';
 }
