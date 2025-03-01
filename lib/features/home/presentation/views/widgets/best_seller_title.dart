b73c18cd491a44a04f26b2db9675ca7a7771a986
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-01 23:36:53 +0200

    remove dead code

--- lib/features/home/presentation/views/widgets/best_seller_title.dart
+++ lib/features/home/presentation/views/widgets/best_seller_title.dart
@@ -1,5 +1,4 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerTitle extends StatelessWidget {
@@ -26,28 +25,6 @@ class BestSellerTitle extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-    return const Padding(
-      padding: EdgeInsets.only(
-        left: 30,
-        right: 30,
-        top: 20,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            child: BestSellerListView(),
-          ),
-        ],
       ),
     );
   }
