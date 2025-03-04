a1a4da8fe1993c731f3538312e36d47dee06531a
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-04 06:15:22 +0200

    change the aspect ratio and make border radius modifiable

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -2,15 +2,20 @@ import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  const CustomBookImage({
+    super.key,
+    this.borderRadius = 16,
+  });
+
+  final double borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 11 / 15,
+      aspectRatio: 10 / 15,
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
+          borderRadius: BorderRadius.circular(borderRadius),
           color: Colors.red,
           image: const DecorationImage(
             fit: BoxFit.cover,
