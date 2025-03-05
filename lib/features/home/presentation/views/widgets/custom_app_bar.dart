c5c38a65ac73d8ec56b58cfdf5b6680032e93402
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-05 21:36:20 +0200

    navigate to search view

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,7 +1,9 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
@@ -21,7 +23,10 @@ class CustomAppBar extends StatelessWidget {
           children: [
             SvgPicture.asset(AssetsData.logo),
             IconButton(
-              onPressed: () {},
+              splashRadius: 24,
+              onPressed: () async {
+                await GoRouter.of(context).push(AppRouter.kSearchView);
+              },
               icon: SvgPicture.asset(
                 AssetsData.search,
               ),
