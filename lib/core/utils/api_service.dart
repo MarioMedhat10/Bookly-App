5fdbc00f3c4013475911bc98b8629b6efa3c7815
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-07 23:58:31 +0200

    create api service with get method

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  ApiService(this.dio);
+
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio;
+
+  Future<Map<String, dynamic>> get({required String endpoint}) async {
+    Response response = await dio.get('_baseUrl$endpoint');
+
+    return response.data;
+  }
+}
