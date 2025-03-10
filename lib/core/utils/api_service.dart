b79635eec19463492432cf2289a12aa1c35ef8f7
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-11 00:48:17 +0200

    make dio private and use baseUrl

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,13 +1,13 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  ApiService(this.dio);
+  ApiService(this._dio);
 
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
-  final Dio dio;
+  final Dio _dio;
 
   Future<Map<String, dynamic>> get({required String endpoint}) async {
-    Response response = await dio.get('_baseUrl$endpoint');
+    Response response = await _dio.get('$_baseUrl$endpoint');
 
     return response.data;
   }
