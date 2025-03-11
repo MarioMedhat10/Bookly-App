bd59b1e7596d4720085428b218ce075c2aa950ea
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-11 05:12:17 +0200

    create newest books cubit and states

---
+++ lib/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitialState());
+}
