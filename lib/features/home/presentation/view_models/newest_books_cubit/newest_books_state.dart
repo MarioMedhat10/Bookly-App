a69541b5548438915bbf3a6ed65348a99aa42910
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-11 05:16:02 +0200

    add newest books states

--- lib/features/home/presentation/view_models/newest_books_cubit/newest_books_state.dart
+++ lib/features/home/presentation/view_models/newest_books_cubit/newest_books_state.dart
@@ -1,6 +1,25 @@
 part of 'newest_books_cubit.dart';
 
 @immutable
-sealed class NewestBooksState {}
+sealed class NewestBooksState extends Equatable {
+  const NewestBooksState();
+
+  @override
+  List<Object?> get props => [];
+}
 
 final class NewestBooksInitialState extends NewestBooksState {}
+
+final class NewestBooksSuccessState extends NewestBooksState {
+  final List<BookModel> books;
+
+  const NewestBooksSuccessState(this.books);
+}
+
+final class NewestBooksLoadingState extends NewestBooksState {}
+
+final class NewestBooksFailureState extends NewestBooksState {
+  final String errMessage;
+
+  const NewestBooksFailureState(this.errMessage);
+}
