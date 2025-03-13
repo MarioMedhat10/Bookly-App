4deaf871bb2566f4fa4163812a73594a4a4e34c6
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-13 02:59:34 +0200

    add BlocBuilder to FeaturedListView

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,35 +1,51 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/view_models/featured_books_cubit/featured_book_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedListView extends StatelessWidget {
   const FeaturedListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 32,
-      ),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .29,
-        child: ListView.separated(
-          separatorBuilder: (context, index) => const SizedBox(
-            width: 15,
-          ),
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(
-                left: index == 0 ? kHorizontalPadding : 0,
-                right: index == 9 ? kHorizontalPadding : 0,
+    return BlocBuilder<FeaturedBookCubit, FeaturedBookState>(
+      builder: (context, state) {
+        if (state is FeaturedBookSuccessState) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 32,
+            ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * .29,
+              child: ListView.separated(
+                separatorBuilder: (context, index) => const SizedBox(
+                  width: 15,
+                ),
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: index == 0 ? kHorizontalPadding : 0,
+                      right: index == 9 ? kHorizontalPadding : 0,
+                    ),
+                    child: const CustomBookImage(),
+                  );
+                },
               ),
-              child: const CustomBookImage(),
-            );
-          },
-        ),
-      ),
+            ),
+          );
+        } else if (state is FeaturedBookFailureState) {
+          return CustomErrorWidget(
+            errMessage: state.errMessage,
+          );
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
