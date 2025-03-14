1af885018510fee508e8a2ddcf479a43da18d89b
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-14 15:14:59 +0200

    provide newest books cubit using bloc builder

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,24 +1,46 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BestSellerListView extends StatelessWidget {
   const BestSellerListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate(
-        (context, index) => const Padding(
-          padding: EdgeInsets.only(
-            bottom: 20,
-            right: kHorizontalPadding,
-            left: kHorizontalPadding,
-          ),
-          child: BestSellerListViewItem(),
-        ),
-        childCount: 10,
-      ),
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccessState) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 20,
+                  right: kHorizontalPadding,
+                  left: kHorizontalPadding,
+                ),
+                child: BestSellerListViewItem(
+                  bookModel: state.books[index],
+                ),
+              ),
+              childCount: state.books.length,
+            ),
+          );
+        } else if (state is NewestBooksFailureState) {
+          return SliverToBoxAdapter(
+            child: CustomErrorWidget(
+              errMessage: state.errMessage,
+            ),
+          );
+        } else {
+          return const SliverFillRemaining(
+            child: CustomLoadingIndicator(),
+          );
+        }
+      },
     );
   }
 }
