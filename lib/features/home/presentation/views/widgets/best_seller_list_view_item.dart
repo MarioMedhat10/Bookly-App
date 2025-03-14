fbecadc4224673640273d85893e5b054592d32b4
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-14 15:16:36 +0200

    display book model data

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,13 +1,19 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
+  const BestSellerListViewItem({
+    super.key,
+    required this.bookModel,
+  });
+
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
@@ -19,20 +25,25 @@ class BestSellerListViewItem extends StatelessWidget {
         height: 105,
         child: Row(
           children: [
-            AspectRatio(
-              aspectRatio: 2 / 3,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.red,
-                  image: const DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      AssetsData.testImage,
-                    ),
-                  ),
-                ),
-              ),
+            // AspectRatio(
+            //   aspectRatio: 2 / 3,
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(8),
+            //       color: Colors.red,
+            //       image: const DecorationImage(
+            //         fit: BoxFit.cover,
+            //         image: AssetImage(
+            //           AssetsData.testImage,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            CustomBookImage(
+              borderRadius: 8,
+              imageUrl:
+                  bookModel.volumeInfo!.imageLinks!.thumbnail ?? noImageUrl,
             ),
             const SizedBox(
               width: 30,
@@ -44,7 +55,7 @@ class BestSellerListViewItem extends StatelessWidget {
                   SizedBox(
                     width: MediaQuery.of(context).size.width * 0.5,
                     child: Text(
-                      'Harry Potter and the Golbet of Fire',
+                      bookModel.volumeInfo!.title!,
                       style: Styles.textStyle20.copyWith(
                         fontFamily: kGtSectraFine,
                         fontWeight: FontWeight.bold,
@@ -56,8 +67,8 @@ class BestSellerListViewItem extends StatelessWidget {
                   const SizedBox(
                     height: 3,
                   ),
-                  const Text(
-                    'J.K. Rowling',
+                  Text(
+                    bookModel.volumeInfo!.authors![0],
                     style: Styles.textStyle14,
                   ),
                   const SizedBox(
@@ -66,13 +77,16 @@ class BestSellerListViewItem extends StatelessWidget {
                   Row(
                     children: [
                       Text(
-                        '19.99 €',
+                        'Free',
                         style: Styles.textStyle20.copyWith(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       const Spacer(),
-                      const BookRating(),
+                      BookRating(
+                        averageRating: bookModel.volumeInfo!.averageRating ?? 0,
+                        ratingCount: bookModel.volumeInfo!.ratingsCount ?? 0,
+                      ),
                     ],
                   ),
                 ],
