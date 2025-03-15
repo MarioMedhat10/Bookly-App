55517de489d64b6662a130239a826b4352e70ea8
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-15 21:52:19 +0200

    implement fetchSimilarBooks method

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -60,4 +60,31 @@ class HomeRepoImpl implements HomeRepo {
       );
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
+      Map<String, dynamic> data = await apiService.get(
+        endpoint:
+            'volumes?Filtering=free-ebooks&Sorting=relevence&q=subject:programming',
+      );
+
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } on DioException catch (e) {
+      return left(
+        ServerFailure.fromDioException(e),
+      );
+    } catch (e) {
+      return left(
+        ServerFailure(e.toString()),
+      );
+    }
+  }
 }
