9cb5b4d3d5d3b986615eea2cd26596a3af53549b
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-15 22:04:17 +0200

    add fetch similar books method

--- lib/features/home/presentation/view_models/similar_books/similar_books_cubit.dart
+++ lib/features/home/presentation/view_models/similar_books/similar_books_cubit.dart
@@ -1,8 +1,24 @@
-import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'similar_books_state.dart';
 
 class SimilarBooksCubit extends Cubit<SimilarBooksState> {
-  SimilarBooksCubit() : super(SimilarBooksInitial());
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitialState());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchSimilarBooks({required String category}) async {
+    emit(SimilarBooksLoadingState());
+    var result = await homeRepo.fetchSimilarBooks(category: category);
+
+    result.fold((failure) {
+      emit(SimilarBooksFailureState(failure.errMessage));
+    }, (books) {
+      emit(SimilarBooksSuccessState(books));
+    });
+  }
 }
