07b6bfe2716aced713a40839c760266a7d53c19b
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-16 05:29:14 +0200

    use launchCustomUrl function

--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,7 +1,7 @@
+import 'package:bookly_app/core/utils/functions/launch_url.dart';
 import 'package:bookly_app/core/widgets/custom_bottom.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
@@ -30,11 +30,10 @@ class BooksAction extends StatelessWidget {
           Expanded(
             child: CustomButton(
               onPressed: () async {
-                Uri uri = Uri.parse(bookModel.volumeInfo!.previewLink!);
-
-                if (!await launchUrl(uri)) {
-                  throw Exception('Could not launch $uri');
-                }
+                await launchCustomUrl(
+                  context,
+                  bookModel.volumeInfo!.previewLink,
+                );
               },
               text: 'Free preview',
               fontSize: 16,
