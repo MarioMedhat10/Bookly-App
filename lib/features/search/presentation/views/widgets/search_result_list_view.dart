71e9a8aa9bd1685523b9b4fe7de5ebb64801f696
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-18 02:08:28 +0200

    provide search cubit using bloc builder

--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,23 +1,41 @@
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/newest_books_list_view_item.dart';
+import 'package:bookly_app/features/splash/presentation/view_models/search_cubit/search_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      physics: const BouncingScrollPhysics(),
-      padding: const EdgeInsets.only(
-        bottom: 30,
-      ),
-      itemBuilder: (context, index) {
-        // return NewestBooksListViewItem();
-        return const Text('');
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        if (state is SearchSuccessState) {
+          return ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.only(
+              bottom: 30,
+            ),
+            itemBuilder: (context, index) {
+              return NewestBooksListViewItem(
+                bookModel: state.books[index],
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 20,
+            ),
+            itemCount: state.books.length,
+          );
+        } else if (state is SearchFailureState) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else if (state is SearchLoadingState) {
+          return const CustomLoadingIndicator();
+        } else {
+          return const Text('');
+        }
       },
-      separatorBuilder: (context, index) => const SizedBox(
-        height: 20,
-      ),
-      itemCount: 10,
     );
   }
 }
