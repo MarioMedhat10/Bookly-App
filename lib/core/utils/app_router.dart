2a6e484d6da27bc659f1d207024c5214e0f15608
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-18 02:05:49 +0200

    provide SearchCubit for SearchView

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -6,6 +6,8 @@ import 'package:bookly_app/features/home/presentation/view_models/similar_books/
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/search/presentation/views/search_view.dart';
+import 'package:bookly_app/features/splash/data/repos/search_repo_impl.dart';
+import 'package:bookly_app/features/splash/presentation/view_models/search_cubit/search_cubit.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -52,7 +54,12 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kSearchView,
-        builder: (context, state) => const SearchView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SearchCubit(
+            ServiceLocator.getIt.get<SearchRepoImpl>(),
+          ),
+          child: const SearchView(),
+        ),
       ),
     ],
   );
