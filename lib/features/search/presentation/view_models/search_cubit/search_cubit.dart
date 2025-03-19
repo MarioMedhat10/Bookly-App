12c968c873c562605d4d2798d454ce7b17229cfd
MarioMedhat10 <mariomedhat4g@gmail.com>
2025-03-20 00:51:26 +0200

    move data and view_model layers to search feature and resolve imports

--- lib/features/search/presentation/view_models/search_cubit/search_cubit.dart
+++ lib/features/search/presentation/view_models/search_cubit/search_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/core/utils/models/book_model/book_model.dart';
-import 'package:bookly_app/features/splash/data/repos/search_repo.dart';
+import 'package:bookly_app/features/search/data/repos/search_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
--- lib/features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/widgets/custom_error_widget.dart';
 import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/newest_books_list_view_item.dart';
-import 'package:bookly_app/features/splash/presentation/view_models/search_cubit/search_cubit.dart';
+import 'package:bookly_app/features/search/presentation/view_models/search_cubit/search_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
